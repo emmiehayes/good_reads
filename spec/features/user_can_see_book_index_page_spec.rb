76ff3bf11a5ccfcc08e70108ac9b2c028477f9ce
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-13 10:20:16 -0600

    getting expected error- moving on

--- spec/features/user_can_see_book_index_page_spec.rb
+++ spec/features/user_can_see_book_index_page_spec.rb
@@ -1,14 +1,14 @@
-describe 'as a user'
-  context 'when visiting /books'
-    it 'can see a list of all books'
-      book_1 = Book.create!(title: 'book number 1')
-      book_2 = Book.create!(title: 'book number 2')
+require 'rails_helper'
 
-      visits books_path
+describe 'as a user visiting /books' do
+  it 'can see a list of all books' do
+    book_1 = Book.create!(title: 'book number 1')
+    book_2 = Book.create!(title: 'book number 2')
 
-      expect(page).to have_content("All Books")
-      expect(page).to have_content(book_1.title)
-      expect(page).to have_content(book_2.title)
-    end
+    visit books_path
+
+    expect(page).to have_content("All Books")
+    expect(page).to have_content(book_1.title)
+    expect(page).to have_content(book_2.title)
   end
 end
