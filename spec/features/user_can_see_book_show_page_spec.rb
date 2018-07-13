bfb990cf730245a7b903f9f127b9546a6fb62dca
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-13 10:17:17 -0600

    add show page test to feature spec file

---
+++ spec/features/user_can_see_book_show_page_spec.rb
@@ -0,0 +1,34 @@
+# As a Visitor,
+# When I visit a book show page,
+# I also see a rating that each user gave the book as part of their review.
+#
+# (rating will be a number between 1 and 5)
+# As a Visitor,
+# When I visit a book show page,
+# I see the average rating for that book.
+#
+# (e.g "Average Rating: 3.5")
+# As a Visitor,
+# When I visit a book show page,
+# I see a heading that includes the highest rating for that book.
+#
+# (e.g "Highest Rating: 4").
+# As a Visitor,
+# When I visit a book show page,
+# under the heading for highest rating,
+# I see the review text, and the name of the reviewer
+# that submitted that review.
+#
+# (it's okay if the review is also listed again on the page later with all other reviews)
+# As a Visitor,
+# When I visit a book show page,
+# I see a header that includes the lowest rating for that book.
+#
+# (e.g "Lowest Rating: 1")
+# As a User,
+# When I visit the book show page,
+# under the header for lowest rating,
+# I see the body of that review and the name of the reviewer
+# that submitted that review.
+#
+# (it's okay if the review is also listed again on the page later with all other reviews)
